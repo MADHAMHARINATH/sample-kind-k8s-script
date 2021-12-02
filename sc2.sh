9bb70099f52a1768e79c6690ac887fd64ccf265a
MADHAMHARINATH <92628693+MADHAMHARINATH@users.noreply.github.com>
2021-12-02 18:01:10 +0530

    Update sc2.sh

--- sc2.sh
+++ sc2.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.11.1/kind-linux-amd64
-chmod +x ./kind
-sudo mv ./kind /usr/bin/kind
-curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
-sudo apt-get update
-sudo apt-get install docker.io
-sudo kind create cluster --config kind-config.yaml
-sudo kind get nodes
-sudo docker ps -a
-kubectl get nodes
+curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.11.1/kind-linux-amd64   #download and install kind
+chmod +x ./kind                                                        #give all permissions for owner 
+sudo mv ./kind /usr/bin/kind                                           #move to any system path if needed
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"       #Download the latest release with the command
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl    #install kubectl     
+sudo apt-get update                                                    
+sudo apt-get install docker.io                                         #install docker
+sudo kind create cluster --config kind-config.yaml                     #create 3node(1-Master,2-Node) Cluster using kind-config.yaml file
+sudo kind get nodes                                                    #to see the list of nodes
+sudo docker ps -a                                                      #to see the list of docker container nodes
+kubectl get nodes                                                      ##to see the list of nodes
