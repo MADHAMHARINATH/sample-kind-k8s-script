1f0e1021667b5830e046030e1ed32bfa8fa2330d
MADHAMHARINATH <92628693+MADHAMHARINATH@users.noreply.github.com>
2021-12-02 18:29:01 +0530

    Update sc2.sh

--- sc2.sh
+++ sc2.sh
@@ -2,7 +2,7 @@
 curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.11.1/kind-linux-amd64   #download and install kind
 chmod +x ./kind                                                        #give all permissions for owner 
 sudo mv ./kind /usr/bin/kind                                           #move to any system path if needed
-curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"       #Download the latest release with the command
+curl -LO https://dl.k8s.io/release/v1.20.0/bin/linux/amd64/kubectl       #Download the latest release with the command
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl    #install kubectl     
 sudo apt-get update                                                    
 sudo apt-get install docker.io                                         #install docker
