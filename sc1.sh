1ec28da77964d8ac93c5b794d346aa22a36b61fd
MADHAMHARINATH <92628693+MADHAMHARINATH@users.noreply.github.com>
2021-12-02 17:05:35 +0530

    Create sc1.sh

---
+++ sc1.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.11.1/kind-linux-amd64
+chmod +x ./kind
+sudo mv ./kind /usr/bin/kind
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+sudo apt-get update
+sudo apt-get install docker.io
+touch kind-config.yaml
