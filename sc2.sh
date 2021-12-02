d59dd9c997590828a9874fe42ac6ec5bdfb315e2
MADHAMHARINATH <92628693+MADHAMHARINATH@users.noreply.github.com>
2021-12-02 22:16:35 +0530

    Update sc2.sh

--- sc2.sh
+++ sc2.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.11.1/kind-linux-amd64   #download and install kind
+curl -Lo ./kind https://kind.sigs.k8s.io/dl/v1.20.7/kind-linux-amd64   #download and install kind
 chmod +x ./kind                                                        #give all permissions for owner 
 sudo mv ./kind /usr/bin/kind                                           #move to any system path if needed
 curl -LO https://dl.k8s.io/release/v1.20.0/bin/linux/amd64/kubectl       #Download the latest release with the command
@@ -9,3 +9,4 @@ sudo apt-get install docker.io                                         #install 
 sudo kind create cluster --config kind-config.yaml                     #create 3node(1-Master,2-Node) Cluster using kind-config.yaml file
 sudo kind get nodes                                                    #to see the list of nodes
 sudo docker ps -a                                                      #to see the list of docker container nodes
+sudo kubectl get nodes
